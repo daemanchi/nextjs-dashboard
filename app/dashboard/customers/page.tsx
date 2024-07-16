316a8651df60c352ba7d985883fd33903e302b21
Selma <heart@selma.dance>
2024-07-16 18:25:55 +0900

    Fix: Customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,20 @@
+import Table from '@/app/ui/customers/table';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+
+  return (
+    <Table query={query} />
+  );
 }
